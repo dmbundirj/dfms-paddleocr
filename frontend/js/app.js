bbb1c402d60b1b8fbdf10ba29c33cdfb5d5f2506
dmbundirj <dmbundi.rj@gmail.com>
2026-07-05 10:12:00 +0530

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,11 @@
- 
+ import { CONFIG } from "./config.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.title = CONFIG.APP.NAME;
+
+    console.log(
+        `${CONFIG.APP.NAME} ${CONFIG.APP.VERSION} Loaded`
+    );
+
+});

50af09aac8cd989c8799cf2889927c7e92ddd57e
dmbundirj <dmbundi.rj@gmail.com>
2026-07-05 10:11:26 +0530

    Update firebase.js

--- frontend/js/firebase.js
+++ frontend/js/firebase.js
@@ -1,1 +1,12 @@
- 
+ import { CONFIG } from "./config.js";
+
+// Firebase configuration
+const firebaseConfig = CONFIG.FIREBASE;
+
+// TODO:
+// initializeApp(firebaseConfig);
+// getAuth();
+// getFirestore();
+// getStorage();
+
+export default firebaseConfig;
